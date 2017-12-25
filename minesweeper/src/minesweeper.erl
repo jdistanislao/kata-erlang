6258365030a8e6498f53c27120ac8ad461a6118b
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2017-12-25 13:57:51 +0100

    Add single row field with one mine

--- minesweeper/src/minesweeper.erl
+++ minesweeper/src/minesweeper.erl
@@ -1,9 +1,25 @@
 -module(minesweeper).
 
--export([new/0, get/2]).
+-export([new/1, get/2, set_mine/2]).
 
-new() ->
-  0.
+-define(MINE_VALUE, -1000).
+-define(MINE_STR, "*").
 
-get(_I, _F) ->
-  {ok, 0}.
+new(S) ->
+  array:new([{size,S}, {fixed,true}, {default,0}]).
+
+set_mine(I, F) ->
+  array:set(I, ?MINE_VALUE, F).
+
+get(I, _F) when I < 0 ->
+  {error, out_of_bound};
+get(I, F) ->
+  V = array:get(I, F),
+  case V == ?MINE_VALUE of
+    true -> {ok, ?MINE_STR};
+    _    -> {ok, V}
+  end.
+
+%%
+%% Utils
+%%
--- minesweeper/test/minesweeper_tests.erl
+++ minesweeper/test/minesweeper_tests.erl
@@ -2,6 +2,17 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-single_element_no_mines_test() ->
-  F = minesweeper:new(),
-  ?assertEqual({ok, 0}, minesweeper:get(0, F)).
+invalid_index_test() ->
+  F = minesweeper:new(1),
+  ?assertEqual({error, out_of_bound}, minesweeper:get(-1, F)).
+
+single_row_no_mines_test() ->
+  F = minesweeper:new(10),
+  ?assertEqual({ok, 0}, minesweeper:get(0, F)),
+  ?assertEqual({ok, 0}, minesweeper:get(9, F)).
+  
+single_row_with_one_mine_at_beginning_test() ->
+  New = minesweeper:new(10),
+  F = minesweeper:set_mine(0, New),
+  ?assertEqual({ok, "*"}, minesweeper:get(0, F)),
+  ?assertEqual({ok, 0}, minesweeper:get(2, F)).
