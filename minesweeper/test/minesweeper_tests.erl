10f8d9b6e4d392b58c290c182aaa7879d73398dc
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2017-12-25 15:25:01 +0100

    Add test with multiple mines on a single row field

--- minesweeper/test/minesweeper_tests.erl
+++ minesweeper/test/minesweeper_tests.erl
@@ -17,3 +17,13 @@ single_row_with_one_mine_at_beginning_test() ->
   ?assertEqual({ok, "*"}, minesweeper:get(0, F)),
   ?assertEqual({ok, 1}, minesweeper:get(1, F)),
   ?assertEqual({ok, 0}, minesweeper:get(2, F)).
+
+single_row_with_multiple_mines_test() ->
+  New = minesweeper:new(6, [0,2,3]),
+  F = minesweeper:find_mines(New),
+  ?assertEqual({ok, "*"}, minesweeper:get(0, F)),
+  ?assertEqual({ok, "*"}, minesweeper:get(2, F)),
+  ?assertEqual({ok, "*"}, minesweeper:get(3, F)),
+  ?assertEqual({ok, 2}, minesweeper:get(1, F)),
+  ?assertEqual({ok, 1}, minesweeper:get(4, F)),
+  ?assertEqual({ok, 0}, minesweeper:get(5, F)).
