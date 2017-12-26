450de752ede98a601b7bcf4d313f9e963b4900a9
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2017-12-26 16:47:54 +0100

    Fix X,Y coord bug

--- minesweeper/src/minesweeper.erl
+++ minesweeper/src/minesweeper.erl
@@ -1,14 +1,10 @@
 -module(minesweeper).
 
--export([new/2, new/3, get/2, find_mines/1]).
+-export([new/3, get/2, find_mines/1]).
 -export([set_mine/2]).
 
 -define(MINE_VALUE, -1000).
 -define(MINE_STR, "*").
-
-new(S, M) ->
-  F = array:new([{size,S}, {fixed,true}, {default,0}]),
-  lists:foldl(fun minesweeper:set_mine/2, F, M).
 
 %%
 %% 0,0 (x,y) at top-left corner
@@ -34,47 +30,38 @@ find_mines(F) ->
       _    -> cell_points(X, Y, V, F)
     end
   end,
-  array:map(fun(Y, A) -> array:map(fun(X, V) -> Find(X, Y, V) end, A) end, F).
+  MapRowFn = fun(X, A) -> array:map(fun(Y, V) -> Find(X, Y, V) end, A) end,
+  array:map(MapRowFn, F).
 
 
 %%
 %% Utils
 %%
 
+%% print(F) ->
+%%   R = array:to_list(F),
+%%   X = fun(A) ->
+%% 	  C = array:to_list(A),
+%% 	  lists:foreach(fun(V) -> io:format("~p", [V]) end, C),
+%% 	  io:format("~n")
+%%       end,
+%%   io:format("~n"),
+%%   lists:foreach(X, R).
+
 cell_points(X, Y, V, F) ->
   Coords = coords_around(X, Y),
-  SumFn = fun({Xc, Yc}, Acc) -> sum(Acc, get_value(Yc, array_get(Xc, F))) end,
+  SumFn = fun({Xc, Yc}, Acc) -> sum(Acc, get(Yc, get(Xc, F, array:new()), 0)) end,
   lists:foldl(SumFn, V, Coords).
 
 coords_around(X, Y) ->
-  C = [ 
-    {X, Y-1},
-    {X+1, Y-1},
-    {X+1, Y},
-    {X+1, Y+1},
-    {X, Y+1},
-    {X-1, Y+1},
-    {X-1, Y},
-    {X-1, Y-1}
-  ],
-  io:format("~p~n", [C]),
-  C.
-  
-array_get(I, _) when I < 0 ->
-  array:new();
-array_get(I, A) ->
+  [{X, Y-1},{X+1, Y-1},{X+1, Y},{X+1, Y+1},{X, Y+1},{X-1, Y+1},{X-1, Y},{X-1, Y-1}].
+
+get(I, _, What) when I < 0 ->
+  What;
+get(I, A, What) ->
   case I < array:size(A) of
     true -> array:get(I, A);
-    _    -> array:new()
-  end.
-
-
-get_value(I, _) when I < 0 ->
-  0;
-get_value(I, F) ->
-  case I < array:size(F) of
-    true -> array:get(I, F);
-    _    -> 0
+    _    -> What
   end.
 
 set_mine({X, Y}, F) ->
--- minesweeper/test/minesweeper_tests.erl
+++ minesweeper/test/minesweeper_tests.erl
@@ -29,6 +29,22 @@ field_with_one_mine_in_the_center_test() ->
   ?assertEqual({ok, 1}, minesweeper:get({2,1}, F)),
   ?assertEqual({ok, 1}, minesweeper:get({2,2}, F)).
 
+
+field_with_one_mine_in_a_corner_test() ->
+  Mines = [{0,2}],
+  New = minesweeper:new(3, 3, Mines),
+  F = minesweeper:find_mines(New),
+  ?assertEqual({ok, 0}, minesweeper:get({0,0}, F)),
+  ?assertEqual({ok, 1}, minesweeper:get({0,1}, F)),
+  ?assertEqual({ok, "*"}, minesweeper:get({0,2}, F)),
+  ?assertEqual({ok, 0}, minesweeper:get({1,0}, F)),
+  ?assertEqual({ok, 1}, minesweeper:get({1,1}, F)),
+  ?assertEqual({ok, 1}, minesweeper:get({1,2}, F)),
+  ?assertEqual({ok, 0}, minesweeper:get({2,0}, F)),
+  ?assertEqual({ok, 0}, minesweeper:get({2,1}, F)),
+  ?assertEqual({ok, 0}, minesweeper:get({2,2}, F)).
+
+
 field_with_mines_except_the_center_test() ->
   Mines = [{0,0},{0,1},{0,2},{1,0},{1,2},{2,0},{2,1},{2,2}],
   New = minesweeper:new(3, 3, Mines),
