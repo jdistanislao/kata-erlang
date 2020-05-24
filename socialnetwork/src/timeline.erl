379a79cbb3ede3d28dd5479e3b04d0862cb317fb
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2020-05-24 13:39:25 +0200

    add alice-only timeline tests

--- socialnetwork/src/timeline.erl
+++ socialnetwork/src/timeline.erl
@@ -4,7 +4,7 @@
 
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([start/1]).
+-export([start/1, get_messages/1, post/2]).
 
 -define(SERVER, ?MODULE).
 
@@ -17,6 +17,12 @@
 start(User) ->
     start_link(User).
 
+get_messages(User) ->
+    gen_server:call(User, {get_messages}).
+
+post(User, Message) ->
+    gen_server:cast(User, {post, Message}).
+
 %%%===================================================================
 %%% Spawning and gen_server implementation
 %%%===================================================================
@@ -28,11 +34,12 @@ init([User]) ->
     State =  #tl_state{user = User, messages = []},
     {ok, State}.
 
-handle_call(_Request, _From, State = #tl_state{}) ->
-    {reply, ok, State}.
+handle_call({get_messages}, _From, State = #tl_state{messages = M}) ->
+    {reply, {ok, M}, State}.
 
-handle_cast(_Request, State = #tl_state{}) ->
-    {noreply, State}.
+handle_cast({post, Message}, State = #tl_state{messages = CurrentMessages}) ->
+    NewState = State#tl_state{messages = [Message|CurrentMessages]},
+    {noreply, NewState}.
 
 handle_info(_Info, State = #tl_state{}) ->
     {noreply, State}.
--- socialnetwork/test/socialnetwork_tests.erl
+++ socialnetwork/test/socialnetwork_tests.erl
@@ -14,14 +14,31 @@ alice_can_create_her_timeline_test() ->
     ?assert(is_pid(Pid)),
     stop_tl(Pid).
 
-%%alice_can_view_her_timeline_test() ->
-%%    ?assert(false).
-%%
+alice_can_view_her_timeline_test() ->
+    {ok, Pid} = start_tl(alice),
+    {ok, Messages} = timeline:get_messages(alice),
+    ?assertMatch([], Messages),
+    stop_tl(Pid).
+
+alice_can_post_messages_to_her_personal_timeline_test() ->
+    {ok, Pid} = start_tl(alice),
+    PostResponse = timeline:post(alice, "first"),
+    {ok, Messages} = timeline:get_messages(alice),
+    ?assertMatch(ok, PostResponse),
+    ?assertMatch(["first"], Messages),
+    stop_tl(Pid).
+
+messages_are_shown_in_reverse_time_order_test() ->
+    {ok, Pid} = start_tl(alice),
+    timeline:post(alice, "first"),
+    timeline:post(alice, "second"),
+    {ok, Messages} = timeline:get_messages(alice),
+    ?assertMatch(["second","first"], Messages),
+    stop_tl(Pid).
+
 %%alice_can_view_bob_timeline_test() ->
 %%    ?assert(false).
 %%
-%%alice_can_post_messages_to_her_personal_timeline_test() ->
-%%    ?assert(false).
-%%
+
 %%alice_could_not_post_messages_to_bob_timeline_test() ->
 %%    ?assert(false).
