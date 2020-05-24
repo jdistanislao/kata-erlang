3bcf774fe67bf20bf86b5ab9619d67cf6f312524
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2020-05-24 14:53:24 +0200

    allow to  start and stop an array of timelines in tests

--- socialnetwork/test/socialnetwork_tests.erl
+++ socialnetwork/test/socialnetwork_tests.erl
@@ -3,42 +3,47 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-start_tl(User) ->
-    timeline:start(User).
+start_tl(Users) ->
+    StartTimelines = fun(User, Pids) ->
+        {ok, Pid} = timeline:start(User),
+        [Pid | Pids]
+        end,
+    lists:foldl(StartTimelines, [], Users).
 
-stop_tl(Tl) ->
-    ?assertMatch(ok, gen_server:stop(Tl)).
+stop_tl(Timelines) ->
+    lists:foreach(fun(Tl) -> ?assertMatch(ok, gen_server:stop(Tl)) end, Timelines).
 
 alice_can_create_her_timeline_test() ->
-    {ok, Pid} = start_tl(alice),
-    ?assert(is_pid(Pid)),
-    stop_tl(Pid).
+    Refs = start_tl([alice]),
+    ?assert(is_pid(lists:last(Refs))),
+    stop_tl(Refs).
 
 alice_can_view_her_timeline_test() ->
-    {ok, Pid} = start_tl(alice),
+    Refs = start_tl([alice]),
     {ok, Messages} = timeline:get_messages(alice),
     ?assertMatch([], Messages),
-    stop_tl(Pid).
+    stop_tl(Refs).
 
 alice_can_post_messages_to_her_personal_timeline_test() ->
-    {ok, Pid} = start_tl(alice),
+    Refs = start_tl([alice]),
     PostResponse = timeline:post(alice, "first"),
     {ok, Messages} = timeline:get_messages(alice),
     ?assertMatch(ok, PostResponse),
     ?assertMatch(["first"], Messages),
-    stop_tl(Pid).
+    stop_tl(Refs).
 
 messages_are_shown_in_reverse_time_order_test() ->
-    {ok, Pid} = start_tl(alice),
+    Refs = start_tl([alice]),
     timeline:post(alice, "first"),
     timeline:post(alice, "second"),
     {ok, Messages} = timeline:get_messages(alice),
     ?assertMatch(["second","first"], Messages),
-    stop_tl(Pid).
+    stop_tl(Refs).
+
+%%alice_could_not_post_messages_to_bob_timeline_test() ->
+%%    ?assert(false).
 
 %%alice_can_view_bob_timeline_test() ->
 %%    ?assert(false).
 %%
 
-%%alice_could_not_post_messages_to_bob_timeline_test() ->
-%%    ?assert(false).
