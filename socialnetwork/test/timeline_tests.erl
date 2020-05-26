9b3919a85187b03592c32c518db23fd112d72a8e
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2020-05-26 18:17:12 +0200

    fix test generators

--- socialnetwork/test/timeline_tests.erl
+++ socialnetwork/test/timeline_tests.erl
@@ -13,22 +13,32 @@
 all_test_() ->
     [
         {"Reading tests", [
-            {"Alice can create her timeline", ?SETUP(fun alice_can_create_her_timeline_test_/1)},
-            {"Alice can view her timeline", ?SETUP(fun alice_can_view_her_timeline_test_/1)},
-            {"Messages are shown in reverse time order", ?SETUP(fun messages_are_shown_in_reverse_time_order_test_/1)}
+            {"Alice can create her timeline",
+                ?SETUP(fun alice_can_create_her_timeline_test_/1)},
+            {"Alice can view her timeline",
+                ?SETUP(fun alice_can_view_her_timeline_test_/1)},
+            {"Messages are shown in reverse time order",
+                ?SETUP(fun messages_are_shown_in_reverse_time_order_test_/1)}
         ]},
         {"Posting tests", [
-            {"Alice can post messages to her personal timeline", ?SETUP(fun alice_can_post_messages_to_her_personal_timeline_test_/1)},
-            {"Alice could not post messages to bob timeline", ?SETUP(fun alice_could_not_post_messages_to_bob_timeline_test_/1)}
+            {"Alice can post messages to her personal timeline",
+                ?SETUP(fun alice_can_post_messages_to_her_personal_timeline_test_/1)},
+            {"Alice could not post messages to bob timeline",
+                ?SETUP(fun alice_could_not_post_messages_to_bob_timeline_test_/1)}
         ]},
         {"Following tests", [
-            {"Charlie can subscribe to Alice timeline", ?SETUP(fun charlie_can_subscribe_to_alice_timeline_test_/1)},
-            {"Prevent multiple subscribe to same timeline", ?SETUP(fun prevent_multiple_subscribe_to_same_timeline_test_/1)}
+            {"Charlie can subscribe to Alice timeline",
+                ?SETUP(fun charlie_can_subscribe_to_alice_timeline_test_/1)},
+            {"Prevent multiple subscribe to same timeline",
+                ?SETUP(fun prevent_multiple_subscribe_to_same_timeline_test_/1)}
         ]},
         {"Direct messages", [
-            {"User can view its private messages", ?SETUP(fun user_can_view_its_private_messages_test_/1)},
-            {"User can't view other users private messages", ?SETUP(fun user_cant_view_other_users_private_messages_test_/1)},
-            {"User can send private messages", ?SETUP(fun user_can_send_private_messages_test_/1)}
+            {"User can view its private messages",
+                ?SETUP(fun user_can_view_its_private_messages_test_/1)},
+            {"User can't view other users private messages",
+                ?SETUP(fun user_cant_view_other_users_private_messages_test_/1)},
+            {"User can send private messages",
+                ?SETUP(fun user_can_send_private_messages_test_/1)}
         ]}
     ].
 
@@ -71,8 +81,10 @@ alice_can_post_messages_to_her_personal_timeline_test_(TlRefs) ->
     [{_, Token}, _, _] = TlRefs,
     PostResponse = timeline:post(alice, Token, "first"),
     GetResponse = timeline:get_messages(alice),
-    ?_assertMatch(ok, PostResponse),
-    ?_assertMatch({ok, ["first"]}, GetResponse).
+    [
+        ?_assertMatch(ok, PostResponse),
+        ?_assertMatch({ok, ["first"]}, GetResponse)
+    ].
 
 alice_could_not_post_messages_to_bob_timeline_test_(TlRefs) ->
     [{_, AliceToken}, _, _] = TlRefs,
@@ -80,8 +92,10 @@ alice_could_not_post_messages_to_bob_timeline_test_(TlRefs) ->
     timeline:post(alice, AliceToken, "first"),
     BobMessages = timeline:get_messages(bob),
     AliceMessages = timeline:get_messages(alice),
-    ?_assertMatch({ok, []}, BobMessages),
-    ?_assertMatch({ok, ["first"]}, AliceMessages).
+    [
+        ?_assertMatch({ok, []}, BobMessages),
+        ?_assertMatch({ok, ["first"]}, AliceMessages)
+    ].
 
 charlie_can_subscribe_to_alice_timeline_test_(TlRefs) ->
     [{_, AliceToken}, {_, BobToken}, {_, CharlieToken}] = TlRefs,
